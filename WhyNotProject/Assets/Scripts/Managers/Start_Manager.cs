4d7d8256f8824ca5c930444696a3297dbbdbda87
Taegon Lee <ltg9amer@naver.com>
2022-06-21 00:07:14 +0900

    06/16 Routine
    UI 옵션 토글 구현(커밋 실수로 재커밋)

--- WhyNotProject/Assets/Scripts/Managers/Start_Manager.cs
+++ WhyNotProject/Assets/Scripts/Managers/Start_Manager.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Start_Manager : MonoBehaviour
 {
+    private OptionUI optionUI;
+
+    private void Start()
+    {
+        optionUI = GameObject.Find("OptionPanel").GetComponent<OptionUI>();
+    }
+
     private void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && optionUI.optionOpened == false)
+        {
+            optionUI.optionOpened = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && optionUI.optionOpened == true)
+        {
+            optionUI.optionOpened = false;
+        }
+        else if (!Input.GetKeyDown(KeyCode.Escape) && optionUI.optionOpened == false)
         {
             if (Input.anyKeyDown)
             {
--- WhyNotProject/Assets/Scripts/UIs/GUI/OptionUI.cs
+++ WhyNotProject/Assets/Scripts/UIs/GUI/OptionUI.cs
@@ -1,10 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 /// <summary>
-/// DOTween으로 패널 구현 예정
+/// 슬라이더를 드래그해서 값을 0을 만들면 다시 값이 변경되지 않음
 /// </summary>
 public class OptionUI : MonoBehaviour
 {
+    public bool optionOpened;
+    [SerializeField] private Slider bgmToggle;
+    [SerializeField] private Slider bgmVolumeSlider;
+    [SerializeField] private Slider sfxToggle;
+    [SerializeField] private Slider sfxVolumeSlider;
+    [SerializeField] private Slider ccToggle;
+    [SerializeField] private TextMeshProUGUI bgmCurrentVolume;
+    [SerializeField] private TextMeshProUGUI sfxCurrentVolume;
 
+    private void Update()
+    {
+        TextUI();
+    }
+
+    public void ToggleUI()
+    {
+        if (bgmToggle.value == 0)
+        {
+            PlayerPrefs.SetFloat("BGMVolume", bgmVolumeSlider.value);
+            bgmVolumeSlider.value = 0;
+        }
+        else
+        {
+            bgmVolumeSlider.value = PlayerPrefs.GetFloat("BGMVolume", 1);
+        }
+
+        if (sfxToggle.value == 0)
+        {
+            PlayerPrefs.SetFloat("SFXVolume", sfxVolumeSlider.value);
+            sfxVolumeSlider.value = 0;
+        }
+        else
+        {
+            sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1);
+        }
+    }
+
+    public void SliderUI()
+    {
+        bgmToggle.value = Mathf.Ceil(bgmVolumeSlider.value);
+        sfxToggle.value = Mathf.Ceil(sfxVolumeSlider.value);
+    }
+
+    private void TextUI()
+    {
+        bgmCurrentVolume.text = $"{Mathf.Floor(bgmVolumeSlider.value * 100)}";
+        sfxCurrentVolume.text = $"{Mathf.Floor(sfxVolumeSlider.value * 100)}";
+    }
 }
