3924ff23c08a946ee622f6bdb992754cf70bd2d2
wingyu0897 <wingyu0897@gmail.com>
2022-10-12 20:27:26 +0900

    Script
    Gimmick Script updated

--- WhyNotProject/Assets/Scripts/Activities/Movements/PostWeight.cs
+++ WhyNotProject/Assets/Scripts/Activities/Movements/PostWeight.cs
@@ -6,54 +6,77 @@ using DG.Tweening;
 public class PostWeight : MonoBehaviour
 {
 	public int maxWeight = 10;
-	public Transform parentObject;
 	public Vector3 basePosition;
 	public Vector3 maxWeightPosition;
-	public List<Collider> objs = new List<Collider>();
-	//public LayerMask interactableLayer;
 
+	public int objCount = 0;
+
+	[SerializeField] private int currentWeight = 0;
+	[SerializeField] private Vector3 boxCastSize;
+	[SerializeField] private Vector3 boxPosition;
+	[SerializeField] private int coinLayer;
+	[SerializeField] private Ease moveEase;
+	[SerializeField] private float moveTime;
+
+	RaycastHit[] hits;
+	RaycastHit[] currentHits;
 	private Vector3 moveAmount;
-	[SerializeField] int currentWeight = 0;
-
-	public Ease moveEase;
 
 	private void Awake()
 	{
-		parentObject = transform.parent;
-		basePosition = parentObject.position;
+		basePosition = transform.position;
+		moveAmount = maxWeightPosition - transform.position;
 
-		moveAmount = maxWeightPosition - parentObject.position;
+		coinLayer = 1 << LayerMask.NameToLayer("Holdable");
 	}
 
-	private void CheckWeight() //무게를 확인하여 위치 조정
+	private void FixedUpdate()
 	{
-		currentWeight = 0;
-		foreach (Collider col in objs)
+		if (objCount != CastCollider())
 		{
-			currentWeight += col.GetComponent<Holdable>().objWeight;
+			ColliderCast();
+		}
+	}
+
+	private int CastCollider()
+	{
+		hits = Physics.BoxCastAll(transform.position + boxPosition, boxCastSize * 0.5f, Vector3.up, Quaternion.identity, 1f, coinLayer);
+
+		return hits.Length;
+	}
+
+	private void ColliderCast()
+	{
+		if (currentHits?.Length > 0)
+		{
+			foreach (RaycastHit obj in currentHits)
+			{
+				obj.transform.SetParent(null);
+			}
+		}
+
+		currentHits = hits;
+
+		print("Cast");
+		objCount = CastCollider();
+
+		currentWeight = 0;
+
+		foreach (RaycastHit col in hits)
+		{
+			col.transform.SetParent(transform);
+			currentWeight += col.transform.GetComponent<Holdable>().objWeight;
 			currentWeight = Mathf.Clamp(currentWeight, 0, maxWeight);
 		}
 
-		parentObject.DOMoveY(basePosition.y + moveAmount.y * (float)(currentWeight / (float)maxWeight), 1f).SetEase(moveEase, 1f);
+		transform.DOMoveY(basePosition.y + moveAmount.y * (float)(currentWeight / (float)maxWeight), moveTime).SetEase(moveEase, 1f);
 	}
 
-	private void OnTriggerEnter(Collider other)
+	private void OnDrawGizmos()
 	{
-		if (other.gameObject.layer == 7)
-		{
-			other.transform.SetParent(parentObject);
-			objs.Add(other);
-			CheckWeight();
-		}
-	}
-
-	private void OnTriggerExit(Collider other)
-	{
-		if (other.gameObject.layer == 2 || other.gameObject.layer == 7)
-		{
-			other.transform.SetParent(null);
-			objs.Remove(other);
-			CheckWeight();
-		}
+		Gizmos.color = Color.magenta;
+		Gizmos.DrawWireCube(transform.position + boxPosition, boxCastSize);
+		Gizmos.DrawLine(transform.position, transform.position + boxPosition);
+		Gizmos.color = Color.white;
 	}
 }
