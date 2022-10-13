14a9c7e0e1d7056d44dada43176da6697226e07e
Taegon Lee <ltg9amer@naver.com>
2022-10-13 20:25:23 +0900

    10/13 Routine
    LightRay 스크립트 수정, 전화부스 프리팹 수정

--- WhyNotProject/Assets/Scripts/Activities/Light/LightRay.cs
+++ WhyNotProject/Assets/Scripts/Activities/Light/LightRay.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class LightRay : MonoBehaviour
 {
-    [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask paperLayer;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
 
     private void Update()
     {
         if (SunRotation.isNight)
         {
-            RaycastHit2D paperHit = Physics2D.Raycast(transform.position, mainCamera.transform.position - transform.position, Mathf.Infinity, paperLayer);
-
-            if (paperHit)
+            if (Physics.Raycast(new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), mainCamera.transform.position - new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), 2.5f, paperLayer))
             {
                 //텍스쳐 바꾸기
             }
 
-            Debug.DrawRay(transform.position, (mainCamera.transform.position - transform.position) * Mathf.Infinity, Color.red);
+            Debug.DrawRay(new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), (mainCamera.transform.position - new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f)) * 2.5f, Color.red);
         }
     }
 }
