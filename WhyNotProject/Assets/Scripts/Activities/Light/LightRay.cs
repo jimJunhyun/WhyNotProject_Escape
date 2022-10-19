1581c8204af40d12deb598455921defacb9570e5
Taegon Lee <ltg9amer@naver.com>
2022-10-19 20:10:10 +0900

    10/19 Routine
    LightRay로 종이 감지 구현

--- WhyNotProject/Assets/Scripts/Activities/Light/LightRay.cs
+++ WhyNotProject/Assets/Scripts/Activities/Light/LightRay.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class LightRay : MonoBehaviour
 {
-    [SerializeField] private LayerMask paperLayer;
+    [SerializeField] private List<Material> materials = new List<Material>();
     private Camera mainCamera;
 
     private void Start()
@@ -14,11 +15,20 @@ public class LightRay : MonoBehaviour
 
     private void Update()
     {
+        RaycastHit paperHit;
+
         if (SunRotation.isNight)
         {
-            if (Physics.Raycast(new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), mainCamera.transform.position - new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), 2.5f, paperLayer))
+            if (Physics.Raycast(new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), mainCamera.transform.position - new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), out paperHit, 2.5f, 1 << 2))
             {
-                //텍스쳐 바꾸기
+                if (paperHit.collider.gameObject.CompareTag("Paper"))
+                {
+                    paperHit.collider.gameObject.GetComponent<MeshRenderer>().material = materials[1];
+                }
+                else
+                {
+                    paperHit.collider.gameObject.GetComponent<MeshRenderer>().material = materials[0];
+                }
             }
 
             Debug.DrawRay(new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f), (mainCamera.transform.position - new Vector3(transform.position.x, GetComponent<Renderer>().bounds.max.y - 0.1f)) * 2.5f, Color.red);
